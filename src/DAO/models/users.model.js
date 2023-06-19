7c95c764a7a32973fc95b1563448463816ecd9ca
JeanPierreCarrey <jeanpierrecarrey@gmail.com>
2023-06-19 20:12:16 -0300

    login

---
+++ src/DAO/models/users.model.js
@@ -0,0 +1,13 @@
+const {Schema, model} = require ("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
+
+const schema = new Schema({
+    firstName: {type: String, required: true, max: 100},
+    lastName: {type: String, required: true, max: 100},
+    email: {type: String, required: true, max: 100, unique: true},
+    password: {type: String, required: true, max: 100},
+    isAdmin: {type: Boolean, required: true},
+});
+
+schema.plugin(mongoosePaginate);
+exports.UserModel = model("users", schema);
--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const {engine} = require('express-handlebars');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const productsRouter = require('./routes/products.router.js');
 const cartsRouter = require('./routes/carts.router.js');
 const viewsRouter = require('./routes/views.router.js');
 const chatRouter = require('./routes/chat.router.js');
+const authRouter = require('./routes/auth.router.js');
 
 const {connectSocket} = require('./utils.js');
 const {connectMongo} = require('./utils.js');
@@ -15,6 +18,13 @@ const port = 8080
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("src/public"));
+app.use(session({
+        store: MongoStore.create({mongoUrl: 'mongodb+srv://jeanpierrecarrey:09lcQ3OehxvKzocQ@backendcoder.nkbcjia.mongodb.net/ecommerce?retryWrites=true&w=majority', ttl: 3600}),
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true,
+    })
+);
 
 app.engine('handlebars', engine());
 app.set('view engine', '.handlebars');
@@ -24,6 +34,7 @@ app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 app.use('/', viewsRouter);
 app.use("/chat", chatRouter);
+app.use('/auth', authRouter);
 app.get("*", (req, res) => {
 	return res.status(404).json({
         status: "error",
@@ -33,8 +44,27 @@ app.get("*", (req, res) => {
 });
 
 const httpServer = app.listen(port, () => {
-    console.log(`Server running on port http://localhost:${port}`)
+    console.log(`Server running on port http://localhost:${port}/auth/login`)
 });
 
 connectMongo();
 connectSocket(httpServer);
+
+/* authRouter.get('/products', (req, res) => {
+    if (req.session.email) {
+        UserModel.findOne({email: req.session.email})
+        .then(user => {
+            if (user) {
+                res.render('products', {user: {firstName: user.firstName, lastName: user.lastName, email: user.email, isAdmin: user.isAdmin}});
+            } else {
+                res.render('products', {user: null});
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            res.render('products', {user: null, error: 'Error retrieving user data'});
+        });
+    } else {
+        res.render('products', {user: null});
+    }
+}); */
