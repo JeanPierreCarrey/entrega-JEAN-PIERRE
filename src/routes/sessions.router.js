86e4688c612355525bf10f27a1e2743b0ae4356d
JeanPierreCarrey <jeanpierrecarrey@gmail.com>
2023-06-28 22:39:50 -0300

    routing fixes

--- src/routes/sessions.router.js
+++ src/routes/sessions.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const sessionsRouter = express.Router();
 
-sessionsRouter.get('/github', passport.authenticate('github', { scope: ['user:email'] }));
+sessionsRouter.get('/login/github', passport.authenticate('github', { scope: ['user:email'] }));
 
 sessionsRouter.get('/githubcallback', passport.authenticate('github', { failureRedirect: '/login' }), (req, res) => {
     req.session.user = req.user;
